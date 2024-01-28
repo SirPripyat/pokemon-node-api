0dbc256dd79303baf87f9d49fddf8dcbe40a13e3
SirPripyat <08.leo.rossi@gmail.com>
2024-01-28 15:56:16 -0300

    Refinando todas as routas de Get para evitar enviar _id do banco de dados para o front end

--- src/evolution-chain/service/get-evolution-chain.service.ts
+++ src/evolution-chain/service/get-evolution-chain.service.ts
@@ -3,21 +3,36 @@ import type { BasicInformation } from "../../types/pokemonBasicInformation";
 import type { Pokemon } from "../../types/pokemon.type";
 import pokemonSchema from "../../pokemon/pokemon.schema";
 import evolutionChainSchema from "../evolution-chain.schema";
+import { GetAllPokemon } from "../../types/getAllPokemon";
 
 export class GetEvolutionChainService {
   public async getEvolutionChain(
     name: BasicInformation["name"],
-  ): Promise<Query<Pokemon[], Pokemon>> {
+  ): Promise<GetAllPokemon[]> {
     const pokemon = await evolutionChainSchema.find({
       name,
     });
 
     const { evolutionChain } = pokemon[0];
 
-    return await pokemonSchema.find({
+    const pokemons = (await pokemonSchema.find({
       "basicInformation.name": {
         $in: evolutionChain,
       },
-    });
+    })) as Pokemon[];
+
+    return pokemons.map(
+      ({
+        basicInformation: { index, name, pokedexNumber, image, pokemonTypes },
+      }) => {
+        return {
+          index,
+          name,
+          pokedexNumber,
+          image,
+          pokemonTypes,
+        };
+      },
+    );
   }
 }
--- src/pokemon/service/get-pokemon.service.ts
+++ src/pokemon/service/get-pokemon.service.ts
@@ -1,9 +1,15 @@
+import type { Pokemon } from "../../types/pokemon.type";
 import pokemonSchema from "../pokemon.schema";
 
 export class GetPokemonService {
-  public async getPokemonByName(name: string) {
-    return await pokemonSchema.findOne({
+  public async getPokemonByName(name: string): Promise<Pokemon> {
+    const pokemon = (await pokemonSchema.findOne({
       "basicInformation.name": name,
-    });
+    })) as Pokemon;
+
+    return {
+      basicInformation: pokemon.basicInformation,
+      baseStats: pokemon.baseStats,
+    };
   }
 }
