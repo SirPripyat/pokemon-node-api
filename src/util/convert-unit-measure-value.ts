10b1f6dc8faf727026093e447b395a3ea7ea66e7
SirPripyat <08.leo.rossi@gmail.com>
2024-02-04 10:56:39 -0300

    criando testes e melhorando a função

--- src/util/convert-unit-measure-value.ts
+++ src/util/convert-unit-measure-value.ts
@@ -1,4 +1,8 @@
 export const convertUnitMeasureValue = (value: number): number => {
-  const convertedValue = value * 0.1;
-  return Number(convertedValue.toFixed(2));
+  const isValidInput = value >= 0 && typeof value === "number";
+  if (!isValidInput) return 0;
+
+  const convertedValue = (value * 0.1).toFixed(2);
+
+  return Number(convertedValue);
 };
